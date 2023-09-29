4f1d664f4bb8faf42cf075b198d15adee1e7fec2
Mortadaal <47819210+Mortadaal@users.noreply.github.com>
2023-09-29 11:55:58 +0200

    sematic UI added

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,17 +1,27 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
+import { Header,List} from 'semantic-ui-react';
 
 function App() {
   const [users, setUsers] = useState([]);
-  useEffect(()=>{
-    axios.get('http://localhost:5000/api/users').then(response=>{
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/users').then(response => {
       setUsers(response.data)
     })
-  },[])
+  }, [])
 
   return (
-    <h1>The Duse Cafe</h1>
+    <div>
+<Header as='h2' icon='coffee' content='The Duse Cafe'/>
+      
+      <List>
+        {users.map((user: any) => (
+          <List.Item key={user.userId}>{user.userName}</List.Item>
+        ))}
+      </List>
+
+    </div>
   )
 }
 
